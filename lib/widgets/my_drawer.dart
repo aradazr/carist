61f97cd98256b52992801357e9b367d24ef01e9f
aradazr <aradazarpanah27@gmail.com>
2024-10-03 15:35:26 +0330

    payment update

--- lib/widgets/my_drawer.dart
+++ lib/widgets/my_drawer.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:myket_iap/myket_iap.dart';
+import 'package:myket_iap/util/iab_result.dart';
 
 import 'package:url_launcher/url_launcher.dart';
 
@@ -13,10 +15,126 @@ class MyDrawer extends StatefulWidget {
 
 class _MyDrawerState extends State<MyDrawer> {
   final List<bool> _isOpen = [false, false, false, false];
+  final String rsaKey =
+      'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDAnnzcOL/YCxnB9Z1VxdYLPswV8LyWAnMKgIUBS8q2oxLZX4Aro0zAnr1wnU0G3q+7ME3qCl8uksYqSwKtGIaGSzS9TBSbJ51wtFL1INtiQFr20SdfqRqOPz7mmgl0gz7T0Vj8PwbU351MlaDsaLvmzivmnR8RHeCOtf2a8UjOpQIDAQAB';
+  String selectDonate = "";
+
+
+
+  //? دونیت انتخاب شده
+  Future<bool> checkConnect() async {
+    try {
+      var iabResult =
+          await MyketIAP.init(rsaKey: rsaKey, enableDebugLogging: true);
+      if (iabResult!.mResponse == 0) {
+        return iabResult.mResponse == 0 ? true : false;
+      } else {
+        showErrorMessage("بررسی کنید که آیا مایکت را نصب دارید.");
+        return false;
+      }
+    } catch (e) {
+      showErrorMessage("مشکلی در ارتباط با مایکت رخ داد");
+      return false;
+    }
+  }
+
+  Future<void> getDonate(String sku) async {
+    try {
+      if (await checkConnect()) {
+        var result =
+            await MyketIAP.launchPurchaseFlow(sku: sku, payload: "حمایت مالی");
+
+        IabResult purchaseResult = result[MyketIAP.RESULT];
+        await MyketIAP.consume(purchase: result[MyketIAP.PURCHASE]);
+        if (purchaseResult.mResponse == 0) {
+          successDialog(
+              'پرداخت با موفقیت انجام شد'); // اینجا یه پیغام موفقیت نمایش میدی مثلا میگی از حمایت شما سپاس گزاریم یا از این چیزا
+        } else {
+          showErrorMessage("متاسفانه پرداخت انجام نشد.");
+        }
+      }
+    } catch (e) {
+      print(e.toString());
+      if (e.toString() == "type 'Null' is not a subtype of type 'Purchase'") {
+        showErrorMessage("پرداخت لغو شد");
+      } else {
+        showErrorMessage("خطایی در ارتباط با مایکت رخ داد.");
+      }
+    }
+  }
+
+  void successDialog(String text) {
+    var snackBar = SnackBar(
+      content: Text(
+        text,
+        textAlign: TextAlign.end,
+      ),
+      backgroundColor: Colors.green,
+      behavior: SnackBarBehavior.fixed,
+      duration: Duration(seconds: 2),
+    );
+    Navigator.of(context).pop(); // بسته شدن Drawer
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String text) {
+    var snackBar = SnackBar(
+      content: Text(
+        text,
+        textAlign: TextAlign.end,
+      ),
+      backgroundColor: Colors.red,
+      behavior: SnackBarBehavior.fixed,
+      duration: Duration(seconds: 2),
+    );
+    Navigator.of(context).pop(); // بسته شدن Drawer
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  Widget _amountButton(Size size, String amount, String donate,StateSetter setState) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          selectDonate = donate;
+        });
+        
+      },
+      child: Container(
+        alignment: Alignment.center,
+        height: size.height / 21,
+        width: size.width / 4,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: selectDonate == donate
+              ? Colors.blue
+              : Colors.black, // تغییر رنگ با توجه به انتخاب
+        ),
+        child: Text(
+          amount,
+          style: const TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  void onConfirm() {
+    getDonate(selectDonate);
+    Navigator.of(context).pop(); // بسته شدن Drawer
+
+    // می‌توانید هر عمل دیگری که لازم است را اینجا انجام دهید.
+  }
+
+  @override
+  void dispose() {
+    MyketIAP.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+
     return BackdropFilter(
       filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
       child: SizedBox(
@@ -77,12 +195,91 @@ class _MyDrawerState extends State<MyDrawer> {
                             children: [
                               InkWell(
                                 radius: 10,
-                                onTap: () async {
-                                  final Uri url =
-                                      Uri.parse('https://zarinp.al/aradazr');
-                                  if (!await launchUrl(url)) {
-                                    throw Exception('Could not launch');
-                                  }
+                                onTap: () {
+                                  // await getDonate();
+
+                                  showModalBottomSheet(
+                                    context: context,
+                                    builder: (context) {
+                                      return StatefulBuilder(
+
+                                        builder: (context, setState) => BackdropFilter(
+                                          filter: ImageFilter.blur(
+                                              sigmaX: 5, sigmaY: 5),
+                                          child: Container(
+                                            height: size.height / 3,
+                                            width: size.width,
+                                            decoration: BoxDecoration(
+                                              color: Colors.white,
+                                              borderRadius: BorderRadius.only(
+                                                  topLeft: Radius.circular(20),
+                                                  topRight: Radius.circular(20)),
+                                            ),
+                                            child: Column(
+                                              children: [
+                                                SizedBox(
+                                                  height: size.height / 20,
+                                                ),
+                                                Text(
+                                                  ':مبلغ مورد نظر خود را انتخاب کنید',
+                                                  style: TextStyle(fontSize: 18),
+                                                ),
+                                                SizedBox(
+                                                  height: size.height / 30,
+                                                ),
+                                                Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.center,
+                                                  children: [
+                                                    _amountButton(size, '20,000T',
+                                                        "donate1",setState),
+                                                    SizedBox(
+                                                      width: size.width / 30,
+                                                    ),
+                                                    _amountButton(size, '40,000T',
+                                                        "donate2",setState),
+                                                  ],
+                                                ),
+                                                SizedBox(
+                                                  height: size.height / 80,
+                                                ),
+                                                _amountButton(
+                                                    size, '60,000T', "donate3",setState),
+                                                SizedBox(
+                                                  height: size.height / 40,
+                                                ),
+                                                InkWell(
+                                                  onTap: () {
+                                                    onConfirm();
+                                                  },
+                                                  child: Container(
+                                                    alignment: Alignment.center,
+                                                    height: size.height / 17,
+                                                    width: size.width / 2.5,
+                                                    decoration: BoxDecoration(
+                                                        border: Border.all(
+                                                            color: Colors.blue,
+                                                            width: 4),
+                                                        borderRadius:
+                                                            BorderRadius.circular(
+                                                                30),
+                                                        color: Colors.black),
+                                                    child: const Text(
+                                                      'تایید',
+                                                      style: TextStyle(
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                          color: Colors.white),
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  );
                                 },
                                 child: Container(
                                   alignment: Alignment.center,
@@ -150,16 +347,32 @@ class _MyDrawerState extends State<MyDrawer> {
                             ),
                           );
                         },
-                        body: Container(
-                          alignment: Alignment.center,
-                          height: size.height / 18,
-                          width: size.width / 2,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white),
-                          child: const Text(
-                            'ارسال لینک',
-                            style: TextStyle(fontWeight: FontWeight.bold),
+                        body: InkWell(
+                          onTap: () {
+                            var snackBar = SnackBar(
+                              content: Text(
+                                '...به زودی',
+                                textAlign: TextAlign.end,
+                              ),
+                              backgroundColor: Colors.blue,
+                              behavior: SnackBarBehavior.fixed,
+                              duration: Duration(seconds: 2),
+                            );
+                            Navigator.of(context).pop(); // بسته شدن Drawer
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          },
+                          child: Container(
+                            alignment: Alignment.center,
+                            height: size.height / 18,
+                            width: size.width / 2,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.white),
+                            child: const Text(
+                              'ارسال لینک',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
                           ),
                         ),
                         isExpanded: _isOpen[1],
@@ -286,58 +499,6 @@ class _MyDrawerState extends State<MyDrawer> {
                             ],
                           ),
                         ),
-                        // body: Column(
-                        //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        //   crossAxisAlignment: CrossAxisAlignment.start,
-                        //   children: [
-                        //     SizedBox(width: size.width),
-                        //     InkWell(
-                        //       onTap: () async {
-                        //         final Uri url =
-                        //             Uri.parse('https://t.me/aradazr');
-                        //         if (!await launchUrl(url)) {
-                        //           throw Exception('Could not launch');
-                        //         }
-                        //       },
-                        //       child: ListTile(
-                        //         tileColor: Colors.redAccent,
-                        //         leading: Image.asset(
-                        //           'assets/images/telegram.png',
-                        //           height: 20,
-                        //         ),
-                        //         title: const Text(
-                        //           'Telegram',
-                        //           style: TextStyle(
-                        //             color: Colors.white,
-                        //             fontSize: 14,
-                        //           ),
-                        //         ),
-                        //       ),
-                        //     ),
-                        //     InkWell(
-                        //       onTap: () async {
-                        //         final Uri url = Uri.parse(
-                        //             'https://mail.google.com/mail/u/0/#inbox');
-                        //         if (!await launchUrl(url)) {
-                        //           throw Exception('Could not launch');
-                        //         }
-                        //       },
-                        //       child: ListTile(
-                        //         leading: Image.asset(
-                        //           'assets/images/gmail.png',
-                        //           height: 35,
-                        //         ),
-                        //         title: const Text(
-                        //           'Gmail',
-                        //           style: TextStyle(
-                        //             color: Colors.white,
-                        //             fontSize: 18,
-                        //           ),
-                        //         ),
-                        //       ),
-                        //     ),
-                        //   ],
-                        // ),
                         isExpanded: _isOpen[3],
                       ),
                     ],
